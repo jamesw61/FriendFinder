baea613262a77bbf777f15518bb79debfb47afd3
James Wick <jmwick2@hotmail.com>
2017-07-12 17:59:16 -0700

    just started

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict'
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+// var fs = require('fs');
+var PORT = 3000;
+const log = x => console.log(x);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json'}));
+app.use(express.static(path.join(__dirname)));
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"/app/public/home.html"));
+});
+
+app.listen(PORT, () => {
+    log(`Listening on ${PORT}`);
+});
+// app.get('/api/:characters?', (req, res) => {
+//     var chosen = req.params.characters;
+//     if (chosen) {
+//         console.log(chosen);
+//         for (let i in characters) {
+//         	// console.log(characters[i].routeName);
+//             if (chosen === characters[i].routeName) {
+//                 return res.json(characters[i]);
+//             }
+//         }
+//         return res.send("No character found");
+//     }
+//     return res.json(characters);
+// });
+
+// app.post("/api/new", (req,res) => {
+// 	var newCharacter = req.body;
+// 	console.log(newCharacter);
+// });
