3130cb8043126e8bd22728701435fd4cac2243b8
James Wick <jmwick2@hotmail.com>
2017-07-18 01:09:29 -0700

    it works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,27 +4,17 @@ var friends = require('../data/friends.js');
 module.exports = (app) => {
     app.post("/new", (req, res) => {
         var user = req.body;
-        console.log(user);
-        // console.log(friends.compare(user));
-
         let tot = 0;
         let minDiff = 100;
         let diff = 0;
         var friendArray = [];
-        var matchedFriend = "n/a";
-        var matchedPhoto = "sldfh";
+        var matchedFriend;
+        var matchedPhoto;
         for (let j in friends) {
-            // console.log(friends[j].name);
             for (let i in user.scores) {
-            	console.log('---')
-            	console.log(friends[j].name);
-                console.log(user.scores[i] + ":" + friends[j].scores[i]);
                 diff = Math.abs(parseInt(user.scores[i]) - parseInt(friends[j].scores[i]));
-                console.log(diff);
                 tot += diff;
-                console.log('---' + tot);
             }
-            console.log(friends[j].name + " total difference: " + tot);
             if (tot < minDiff) {
                 friendArray = [];
                 matchedFriend = friends[j];
@@ -32,24 +22,15 @@ module.exports = (app) => {
                 minDiff = tot;
                 friendArray.push(matchedFriend);
                 }
-            else if (tot = minDiff) {
+            else if (tot == minDiff) {
                 matchedFriend = friends[j];
                 matchedPhoto = friends[j].photo;
-                minDiff = tot;
                 friendArray.push(matchedFriend);
             }
             tot=0;
-
-
-
-
-
-
         }
         friends.push(user);
-        // res.send(matchedFriend);
         res.send(friendArray);
-
     });
 
     app.get('/friends', (req, res) => {
--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 'use strict'
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
-// var friends = require('../data/friends.js');
 
-
-
-// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-// app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
-// app.use(express.static(path.join(__dirname)));
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
 });
-
-
-
